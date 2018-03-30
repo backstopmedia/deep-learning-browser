0e8ee2790bfc6a2b1e4dd233a3cca9beae42826b
Xavier Bourry <xavier.bourry@polytechnique.org>
2018-03-30 17:43:02 +0200

    replace tabs by 4 spaces + finish 6_MNISTimproved example + allow use of half/quarter datasets for MNIST examples

--- chapter3/5_MNIST/script.js
+++ chapter3/5_MNIST/script.js
@@ -6,47 +6,47 @@ direct link : http://neuralnetworksanddeeplearning.com/chap1.html
 
 This is the equivalent of this code in python :
 
-	import mnist_loader
-	training_data, validation_data, test_data =mnist_loader.load_data_wrapper()
-	import network
-	net = network.Network([784, 30, 10])
-	net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+    import mnist_loader
+    training_data, validation_data, test_data =mnist_loader.load_data_wrapper()
+    import network
+    net = network.Network([784, 30, 10])
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 */
 
 //entry point
-function main(){
-	//return;
-	printLog('INFO in script.js : load MNIST data...');
-	setStatus('LOAD DATA...');
-	mnist_loader.load_data_wrapper(function(data){
-		printLog('INFO in script.js : MNIST data is loaded successfully :)');
-		setStatus('DATA LOADED. START TRAINING...');
+function main(dataSetdivideFactor){
+    //return;
+    printLog('INFO in script.js : load MNIST data...');
+    setStatus('LOAD DATA...');
+    mnist_loader.load_data_wrapper(dataSetdivideFactor, function(data){
+        printLog('INFO in script.js : MNIST data is loaded successfully :)');
+        setStatus('DATA LOADED. START TRAINING...');
 
-		//we do not execute start immediately to allow the DOM to update
-		//(for status and logging area)
-		setTimeout(start.bind(null, data), 5);
-	});
+        //we do not execute start immediately to allow the DOM to update
+        //(for status and logging area)
+        setTimeout(start.bind(null, data), 5);
+    });
 }
 
 function start(data){
-	var net = new network.Network([784, 30, 10]);
+    var net = new network.Network([784, 30, 10]);
 
-	var tStart=performance.now();
-	net.SGD(data.training_data, 30/*epochs*/, 8/*mini_batch_size*/, 3.0/*eta*/, data.test_data,
-			function(){ //callback function launched when learning is finished
-				var tEnd=performance.now();
-				printLog('INFO : training duration = '+((tEnd-tStart)/1000).toString()+' seconds');
-			});
+    var tStart=performance.now();
+    net.SGD(data.training_data, 30/*epochs*/, 8/*mini_batch_size*/, 3.0/*eta*/, data.test_data,
+            function(){ //callback function launched when learning is finished
+                var tEnd=performance.now();
+                printLog('INFO : training duration = '+((tEnd-tStart)/1000).toString()+' seconds');
+            });
 }
 
 //logging
 function printLog(msg){
-	console.log(msg);
-	document.getElementById('logArea').value+=msg+'\n';
+    console.log(msg);
+    document.getElementById('logArea').value+=msg+'\n';
 }
 
 //update status
 function setStatus(status){
-	console.log('SET STATUS = ', status);
-	document.getElementById('status').value=status;
+    console.log('SET STATUS = ', status);
+    document.getElementById('status').value=status;
 }
--- chapter3/6_MNISTimproved/script.js
+++ chapter3/6_MNISTimproved/script.js
@@ -6,49 +6,49 @@ direct link : http://neuralnetworksanddeeplearning.com/chap1.html
 
 This is the equivalent of this code in python :
 
-	import mnist_loader
-	training_data, validation_data, test_data =mnist_loader.load_data_wrapper()
-	import network
-	net = network.Network([784, 30, 10])
-	net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+    import mnist_loader
+    training_data, validation_data, test_data =mnist_loader.load_data_wrapper()
+    import network
+    net = network.Network([784, 30, 10])
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 */
 
 "use strict";
 
 //entry point
-function main(){
-	//return;
-	printLog('INFO in script.js : load MNIST data...');
-	setStatus('LOAD DATA...');
-	mnist_loader.load_data_wrapper(function(data){
-		printLog('INFO in script.js : MNIST data is loaded successfully :)');
-		setStatus('DATA LOADED. START TRAINING...');
+function main(dataSetdivideFactor){
+    //return;
+    printLog('INFO in script.js : load MNIST data...');
+    setStatus('LOAD DATA...');
+    mnist_loader.load_data_wrapper(dataSetdivideFactor, function(data){
+        printLog('INFO in script.js : MNIST data is loaded successfully :)');
+        setStatus('DATA LOADED. START TRAINING...');
 
-		//we do not execute start immediately to allow the DOM to update
-		//(for status and logging area)
-		setTimeout(start.bind(null, data), 5);
-	});
+        //we do not execute start immediately to allow the DOM to update
+        //(for status and logging area)
+        setTimeout(start.bind(null, data), 5);
+    });
 }
 
 function start(data){
-	var net = new network.Network([784, 256, 128, 10]);
+    var net = new network.Network([784, 256, 64, 10]);
 
-	var tStart=performance.now();
-	net.SGD(data.training_data, 10/*epochs*/, 8/*mini_batch_size*/, 0.5/*eta*/, data.test_data,
-			function(){ //callback function launched when learning is finished
-				var tEnd=performance.now();
-				printLog('INFO : training duration = '+((tEnd-tStart)/1000).toString()+' seconds');
-			});
+    var tStart=performance.now();
+    net.SGD(data.training_data, 20/*epochs*/, 8/*mini_batch_size*/, 1.0/*eta*/, data.test_data,
+            function(){ //callback function launched when learning is finished
+                var tEnd=performance.now();
+                printLog('INFO : training duration = '+((tEnd-tStart)/1000).toString()+' seconds');
+            });
 }
 
 //logging
 function printLog(msg){
-	console.log(msg);
-	document.getElementById('logArea').value+=msg+'\n';
+    console.log(msg);
+    document.getElementById('logArea').value+=msg+'\n';
 }
 
 //update status
 function setStatus(status){
-	console.log('SET STATUS = ', status);
-	document.getElementById('status').value=status;
+    console.log('SET STATUS = ', status);
+    document.getElementById('status').value=status;
 }
